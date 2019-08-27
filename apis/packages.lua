58add3f8f76bb46a5fd8ca8057a20c03c5bc9385
Collin Read <readcollin0@gmail.com>
2019-08-26 18:14:04 -0700

    Fixed a couple of issues with first tests

--- apis/packages.lua
+++ apis/packages.lua
@@ -12,14 +12,9 @@ function getPackageList()
     local urlCheck, err = http.checkURL(getFileURI(packageList))
     
     if urlCheck == true then
-        local handle = http.get(getFileURI(packageList))
-        local packages = textutils.serialize(handle.readAll())
+        local respHandle = http.get(getFileURI(packageList))
+        local packages = textutils.unserialize(respHandle.readAll())
         
-        for k, v in ipairs(packages) do
-        
-            print(k .. ": " .. v)
-        
-        end
     else
         term.setTextColor(colors.red)
         print("Error connecting to update server.")
