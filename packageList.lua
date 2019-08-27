36f780682ded19bb8fd0fcb0557434dc5d063db1
Collin Read <readcollin0@gmail.com>
2019-08-26 19:10:00 -0700

    Darn, put the slashes in the right spot.

--- packageList.lua
+++ packageList.lua
@@ -5,12 +5,12 @@
         description = "A basic weather manipulator, for the server I play on.",
         files = {
             {
-                dest = "/weather.lua",
+                dest = "weather.lua",
                 origin = "/weather/weather.lua",
             },
             {
                 dest = "apis/buttons.lua",
-                origin = "apis/buttons.lua"
+                origin = "/apis/buttons.lua"
             },
         },
     },
