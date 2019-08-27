6b2848294730a49c12f6c4725c6630fad27c536e
Collin Read <readcollin0@gmail.com>
2019-08-26 19:08:12 -0700

    Added a slash at the start of the file names in PackageList

--- packageList.lua
+++ packageList.lua
@@ -5,8 +5,8 @@
         description = "A basic weather manipulator, for the server I play on.",
         files = {
             {
-                dest = "weather.lua",
-                origin = "weather/weather.lua",
+                dest = "/weather.lua",
+                origin = "/weather/weather.lua",
             },
             {
                 dest = "apis/buttons.lua",
